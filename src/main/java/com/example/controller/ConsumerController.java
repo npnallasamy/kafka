05dab6ba7ac042118eb12b24ae32b2640426a190
npnallasamy <81501931+npnallasamy@users.noreply.github.com>
2021-09-15 16:24:45 +0530

    Add files via upload

---
+++ src/main/java/com/example/controller/ConsumerController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class ConsumerController {
+	
+	@GetMapping("/consumer")
+	public String ConsumerMessage()
+	{
+		return null;
+		
+	}
+
+}
